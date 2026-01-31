259d23621068e68f7ecddf4a4eb20eacfe427ae8
sebastianlonre <sebastian.londono75@correo.tdea.edu.co>
2026-01-31 15:52:52 -0500

    Se agrega estructura de enrutado base, se elimina la carpeta auth, se implementara como funcionalidad de user

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const mainRouter = require('./router')
 const app = express()
 
 const PORT = 3000
@@ -8,6 +9,8 @@ app.use(express.json())
 app.get('/', (req, res) => {
   res.send('hello world')
 })
+
+app.use('/api', mainRouter)
 
 app.listen(PORT, ()=>{
     console.log(`Servidor Escuchando en la ruta http://localhost:${PORT}`)
